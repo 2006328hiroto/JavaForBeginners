31e4e55f0b68219f28d9aabe34db72e55848cd01
hiroto_hiroto <zeal24360046@fsgcl.jp>
2025-05-26 10:05:47 +0900

    色

---
+++ src/RectTest.java
@@ -0,0 +1,16 @@
+
+public class RectTest extends MyFrame {
+	public void run() {
+		setColor(255, 255, 0);
+		fillRect(30, 40, 50, 100);
+		setColor(128, 0, 128);
+		fillRect(130, 40, 50, 100);
+		setColor(0, 255, 255);
+		fillRect(30, 200, 150, 10);
+		setColor(84, 84, 87);
+		fillRect(90, 160, 30, 30);
+		setColor(0, 0, 0);
+		fillRect(90, 200, 30, 30);
+	}
+
+}
